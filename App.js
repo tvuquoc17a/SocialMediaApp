696a202bc31c8c4cb3a8036528d6abfddaef2916
Vu Quoc Tuan <tuanspc@Vus-Mac-mini.local>
2024-07-21 23:48:26 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 50, fontFamily: "Inter Black" }}> Hello Tuan</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ assets/fonts/helper.js
@@ -0,0 +1,20 @@
+const getFontFamily = (baseFont = 'Inter', weight) => {
+  switch (weight){
+    case 100:
+      return `${baseFont}-Thin`
+    case 200:
+      return `${baseFont}-ExtraLight`
+    case 300:
+      return `${baseFont}-Thin`
+    case 400:
+      return `${baseFont}-Thin`
+    case 500:
+      return `${baseFont}-Thin`
+    case 600:
+      return `${baseFont}-Thin`
+    case 700:
+      return `${baseFont}-Thin`
+    case 800:
+      return `${baseFont}-Thin`
+  }
+}
